6d38be8495585ea1d7e2b97dddee24e9ccb102b1
Mayel de Borniol <git@mayel.space>
2024-03-17 17:16:18 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/900

--- lib/components/profile_boosts/profile_boosts_live.ex
+++ lib/components/profile_boosts/profile_boosts_live.ex
@@ -6,11 +6,11 @@ defmodule Bonfire.UI.Reactions.ProfileBoostsLive do
   prop selected_tab, :any, default: "boosts"
   prop smart_input, :boolean, required: true
   prop smart_input_opts, :map, default: %{}
-  prop search_placeholder, :string
-  prop feed_title, :string
+  prop search_placeholder, :string, default: nil
+  prop feed_title, :string, default: nil
   prop user, :map
-  prop feed, :list
-  prop page_info, :any
+  prop feed, :list, default: nil
+  prop page_info, :any, default: nil
   prop follows_me, :boolean, default: false
   prop loading, :boolean, default: false
   prop feed_component_id, :any, default: nil
