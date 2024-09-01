05c4abc4a2bcf8f7cfef1d81862e8da2868f3f96
Mayel de Borniol <git@mayel.space>
2024-09-01 09:34:45 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/986

--- lib/components/pin/pin_action_live.ex
+++ lib/components/pin/pin_action_live.ex
@@ -2,6 +2,7 @@ defmodule Bonfire.UI.Reactions.PinActionLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
   prop object, :any, required: true
+  prop pinned?, :boolean, default: nil
   prop class, :css_class, default: "btn btn-ghost btn-circle btn-sm"
   prop scope, :atom, default: :profile
   prop scope_object, :string, default: nil
