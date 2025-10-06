0e20d29fdc01388e9a67c31f5bae5c7f37a63d41
Mayel de Borniol <git@mayel.space>
2025-10-06 14:28:31 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1581

--- lib/components/boost/boost_action_live.ex
+++ lib/components/boost/boost_action_live.ex
@@ -10,14 +10,14 @@ defmodule Bonfire.UI.Reactions.BoostActionLive do
   prop showing_within, :atom
   prop my_boost, :any, default: nil
   prop parent_id, :any, default: nil
-  prop quote_permission, :any, default: nil
+  # prop quote_permission, :any, default: nil
 
   def update_many(assigns_sockets),
     do: Bonfire.Social.Boosts.LiveHandler.update_many(assigns_sockets, caller_module: __MODULE__)
 
-  # Helper to use preloaded permission or fall back to checking
-  def quote_permission_or_check(nil, user, object),
-    do: Bonfire.Social.Quotes.check_quote_permission(user, object)
+  # # Helper to use preloaded permission or fall back to checking
+  # def quote_permission_or_check(nil, user, object),
+  #   do: Bonfire.Social.Quotes.check_quote_permission(user, object)
 
-  def quote_permission_or_check(permission, _user, _object), do: permission
+  # def quote_permission_or_check(permission, _user, _object), do: permission
 end
